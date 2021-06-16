f98cf83e758cc03926e6c450e10e68ec068d92c3
[grodriguez-dev] <[grodriguez.dev99@gmail.com]>
2021-06-16 13:39:36 -0400

    agregado item de sidebar

---
+++ resources/views/admin/partials/items/080-bannedip.blade.php
@@ -0,0 +1,8 @@
+<li class="">
+    <a href="{{ route('banned_ip.index') }}" class="waves-effect waves-dark">
+        <span class="pcoded-micon">
+            <i class="feather icon-menu"></i>
+        </span>
+        <span class="pcoded-mtext">Banned IP</span>
+    </a>
+</li>
