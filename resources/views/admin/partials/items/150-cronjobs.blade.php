126fd1debf6c7e7a656341d08902f4a0aad5c846
grodriguez-dev <grodriguez.dev99@gmail.com>
2021-07-07 12:05:06 -0400

    agregado intent den el sidebar para el modulo cronjobs

---
+++ resources/views/admin/partials/items/150-cronjobs.blade.php
@@ -0,0 +1,8 @@
+<li class="">
+    <a href="{{ route('cron_jobs.index') }}" class="waves-effect waves-dark">
+        <span class="pcoded-micon">
+            <i class="feather icon-menu"></i>
+        </span>
+        <span class="pcoded-mtext">Cron Jobs</span>
+    </a>
+</li>
