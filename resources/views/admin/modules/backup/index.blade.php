f73a1cf7168dc2948b2fd28a38cffb3cb878f172
grodriguez-dev <grodriguez.dev99@gmail.com>
2021-07-06 20:24:32 -0400

    creado modulo BACKUP, Modulo para la creacion de un backup del sitio

---
+++ resources/views/admin/modules/backup/index.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.layouts.admin')
+@section('content')
+<div class="pcoded-content">
+
+    <div class="page-header card">
+        <div class="row align-items-end">
+            <div class="col-lg-8">
+                <div class="page-header-title">
+                    <i class="feather icon-clipboard bg-c-blue"></i>
+                    <div class="d-inline">
+                        <h5>{{ $title }}</h5>
+                        <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="page-header-breadcrumb">
+                    <ul class=" breadcrumb breadcrumb-title">
+                        <li class="breadcrumb-item">
+                            <a href="index.html"><i class="feather icon-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Form Components</a></li>
+                        <li class="breadcrumb-item">
+                            <a href="#!">{{ $title }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pcoded-inner-content">
+
+        <div class="main-body">
+            <div class="page-wrapper">
+
+                <div class="page-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+
+                            <div class="card">
+                                <div class="card-header">
+                                    <h5>{{ $title }}</h5>
+                                    <span>Add class of <code>.form-control</code> with <code>&lt;input&gt;</code> tag</span>
+                                </div>
+                                <div class="card-block">
+                                    <form action="" method="POST">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <div class="col-sm-6">
+                                                <label class="col-form-label">Backup</label>
+                                                <br>
+                                                <div style="font-size: 13px;">
+                                                    <span>All backups are saved on mucore root/mucore_backup folder.</span>
+                                                </div>
+                                                <div style="font-size: 13px;">
+                                                    <span>backup folder name format: backup_date_time.</span>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-12">
+                                                <a href="#" class="btn btn-primary waves-effect waves-light" style="float: right;">Create Backup</a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div id="styleSelector">
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/partials/items/130-backup.blade.php
@@ -0,0 +1,8 @@
+<li class="">
+    <a href="{{ route('backup.index') }}" class="waves-effect waves-dark">
+        <span class="pcoded-micon">
+            <i class="feather icon-menu"></i>
+        </span>
+        <span class="pcoded-mtext">Backup</span>
+    </a>
+</li>
